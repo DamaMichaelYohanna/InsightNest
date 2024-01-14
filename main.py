649815f021f779e8056db3cef377052723f57675
= <get2dama11@gmail.com>
2024-01-14 14:48:37 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from user.views import user
+
+app = FastAPI()
+app.mount('/user', user)
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+
+
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
