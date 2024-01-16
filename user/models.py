a4fbfbca66a286a6f6b97fb2b1a3682b7eb2b311
= <get2dama11@gmail.com>
2024-01-16 14:27:58 +0100

    added functions to model

--- user/models.py
+++ user/models.py
@@ -1,8 +1,7 @@
 import os
 import pathlib
 from enum import Enum as PyEnum
-
-from sqlmodel import SQLModel, create_engine, Session, Field, Relationship, Enum
+from sqlmodel import SQLModel, create_engine, Session, Field, Relationship, Enum, select
 from typing import List
 
 
@@ -12,26 +11,42 @@ class Status(PyEnum):
     BLOCKED = 0
 
 
-class FollowRelationship(SQLModel, table=True):
+class Associate(SQLModel, table=True):
     class Config:
-        orm_mode = True
+        from_attributes = True
 
     by_id: int = Field(foreign_key='user.id', primary_key=True, description='Relationship from')
     to_id: int = Field(foreign_key='user.id', primary_key=True, description='Relationship to')
-    status: Status | None = None
+    status: int | None = None
 
     # Define the Many-to-Many relationship
-    by: "User" = Relationship(back_populates="relationship_by")
-    to: "User" = Relationship(back_populates="relationship_to")
+    by: "User" = Relationship(
+        sa_relationship_kwargs={
+            "primaryjoin": "Associate.by_id==User.id", })
+    to: "User" = Relationship(
+        sa_relationship_kwargs={
+            "primaryjoin": "Associate.to_id==User.id", }
+    )
 
-    @property
-    def name(self) -> str:
-        return 'Relationship from %s to %s' % (self.by.username, self.to.username)
+    def get_or_create(self, user_id):
+        with Session(engine) as session:
+            filter_statement = select(Associate).where(
+                Associate.by == self,
+                Associate.to == user_id
+            )
+            associate = session.exec(filter_statement)
+            if associate:
+                return associate
+            else:
+                associate = Associate(by=self, to=user_id)
+                session.add(associate)
+                session.commit()
+                return associate
 
 
 class User(SQLModel, table=True):
     class Config:
-        orm_mode = True
+        from_attributes = True
 
     id: int | None = Field(default=None, primary_key=True)
     username: str
@@ -46,22 +61,85 @@ class User(SQLModel, table=True):
     address: str | None = None
     email_verified: bool | None = None
 
-    following: List[Relationship] = Relationship(
+    follow: List["Associate"] = Relationship(
         back_populates="by",
-        sa_relationship=Field(default=None)
+        sa_relationship_kwargs={
+            "primaryjoin": "Associate.by_id==User.id"}
     )
+
+    @property
+    def get_following(self):
+        """return the people the current user is following"""
+        session = get_session()
+        filter_statement = select(Associate).where(Associate.by == self)
+        following = session.exec(filter_statement)
+        return following.all()
+
+    @property
+    def get_followers(self):
+        """return the user that are currently following the user"""
+        session = get_session()
+        filter_statement = select(Associate).where(Associate.to == self)
+        followers = session.exec(filter_statement)
+        return followers.all()
+
+    def follow_user(self, user_2_follower):
+        associate: Associate = Associate(by=self, to=user_2_follower, status=1)
+        session = get_session()
+        session.add(associate)
+        session.commit()
+
+    def unfollow_user(self, user_2_unfollower):
+        filter_statement = select(Associate).where(
+            Associate.by == self,
+            Associate.to == user_2_unfollower,
+            Associate.status == 1
+
+        )
+        session = get_session()
+        return_value = session.exec(filter_statement).one()
+        session.delete(return_value)
+        session.commit()
+
+    def block_user(self, user_2_block):
+        session = get_session()
+        print(type(session))
+        filter_statement: select = select(Associate).where(
+            Associate.by == self,
+            Associate.to == user_2_block,
+            Associate.status==0
+        )
+        return_value = session.exec(filter_statement).one()
+        if return_value:
+            return_value.status = 0
+        else:
+            associate: Associate = Associate(
+                by=self,
+                to=user_2_block,
+                status=0
+            )
+            session.add(associate)
+            session.commit()
+
+    def unblock_user(self, user_2_unblock):
+        filter_statement = select(Associate).where(
+            Associate.by == self,
+            Associate.to == user_2_unblock,
+            Associate.status==1
+
+        )
+        session = get_session()
+        return_value = session.exec(filter_statement).one()
+        session.delete(return_value)
+        session.commit()
 
 
 sql_db_path = os.path.join(pathlib.Path(__file__).parent.parent, "database.db")
 sql_db_link = f"sqlite:///{sql_db_path}"
 engine = create_engine(sql_db_link, echo=True)
+SQLModel.metadata.create_all(bind=engine)
 
 
-def get_db_session():
-    create_db_table()
-    session = Session(engine)
-    return session
-
-
-def create_db_table():
-    SQLModel.metadata.create_all(bind=engine)
+def get_session():
+    with Session(engine) as session:
+        yield session
