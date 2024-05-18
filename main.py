7e0f9de34f7b2e64847e0a46e47e003971b81a68
DamaMichaelYohanna <get2dama11@gmail.com>
2024-05-18 21:36:31 +0100

    updated the main app to add entry for the crime section

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import pathlib
 
 from fastapi import FastAPI
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlmodel import SQLModel, Session
 
 from insight.routing import insight
 from user.views import user
+from crime.routing import crime
+from database import engine
 
 
 app = FastAPI()
-app.mount('/user', user)
+app.mount('/crime', crime)
 app.mount('/insight', insight)
+app.mount('/user', user)
 
 # @app.get("/")
 # async def root():
@@ -22,3 +25,7 @@ app.mount('/insight', insight)
 # @app.get("/hello/{name}")
 # async def say_hello(name: str):
 #     return {"message": f"Hello {name}"}
+# Base class for database models
+
+# This will create the database tables
+SQLModel.metadata.create_all(bind=engine)
