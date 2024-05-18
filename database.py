461aa27e36d6791abf5765e08a04b11050bbde0d
DamaMichaelYohanna <get2dama11@gmail.com>
2024-05-18 21:38:55 +0100

    added database utility file

--- database.py
+++ database.py
@@ -4,22 +4,16 @@ import pathlib
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from sqlmodel import Session
 
 sql_db_path = os.path.join(pathlib.Path(__file__).parent.parent, "database.db")
 sql_db_link = f"sqlite:///{sql_db_path}"
 
 engine = create_engine(sql_db_link, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def get_session():
-    with SessionLocal as session:
+    with Session(engine) as session:
         yield session
 
 
-# Base class for database models
-Base = declarative_base()
-
-# This will create the database tables
-Base.metadata.create_all(bind=engine)
